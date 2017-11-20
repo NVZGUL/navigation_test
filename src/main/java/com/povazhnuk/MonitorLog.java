9e4cd60b394851c81404a4b83fdf461116c969d9
NVZGUL <h3rmess@gmail.com>
2017-11-20 12:36:38 +0300

    added run method, fixed writeToFile method

--- src/main/java/com/povazhnuk/MonitorLog.java
+++ src/main/java/com/povazhnuk/MonitorLog.java
@@ -1,9 +1,7 @@
 package com.povazhnuk;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -27,7 +25,7 @@ public class MonitorLog {
                 .build();
     };
 
-    public List<UserNavigation> run() throws IOException {
+    public List<UserNavigation> readFromFile() throws IOException {
         List<UserNavigation> il;
         InputStream is = new FileInputStream(file);
         BufferedReader br = new BufferedReader(new InputStreamReader(is));
@@ -36,10 +34,52 @@ public class MonitorLog {
         return il;
     }
 
+    public void writeToFile(Set<UserNavigation> lst) throws IOException {
+        File file = new File("avg_" + this.file.getName());
+        BufferedWriter bw = new BufferedWriter(new FileWriter(file));
+        String str = "";
+        for (UserNavigation i: lst) {
+            str += i.toString() + '\n';
+        }
+        bw.write(str);
+        bw.close();
+    }
+
+    public void run() throws IOException {
+        List<UserNavigation> lst = this.readFromFile();
+        lst.sort(Comparator.comparing(UserNavigation::getNumberOfSeconds).reversed());
+        Map<UserNavigation, List<Double>> usrUrlBuffer = new HashMap<>();
+
+        for (UserNavigation i : lst) {
+            List<Double> tmp = new ArrayList<>();
+            if (usrUrlBuffer.containsKey(i)) {
+                tmp = usrUrlBuffer.get(i);
+                tmp.add(i.getNumberOfSeconds());
+                UserNavigation tmpUN = UserNavigation.newBuilder()
+                                        .setUser(i.getUser())
+                                        .setURL(i.getURL())
+                                        .setNumberOfSeconds(this.avarageSeconds(tmp))
+                                        .build();
+                usrUrlBuffer.remove(i);
+                usrUrlBuffer.put(tmpUN, tmp);
+            }
+            else {
+                tmp.add(i.getNumberOfSeconds());
+                usrUrlBuffer.put(i, tmp);
+            }
+        }
+        for (UserNavigation i : usrUrlBuffer.keySet()) {
+            System.out.println(i);
+        }
+        this.writeToFile(usrUrlBuffer.keySet());
+    }
+
+    private double avarageSeconds(List<Double> lst) {
+        return lst.stream().mapToDouble(a -> a).average().getAsDouble();
+    }
+
     public static void main(String[] args) throws IOException{
         MonitorLog mL = new MonitorLog("test.csv");
-        List<UserNavigation> lst = mL.run();
-        lst.sort(Comparator.comparing(UserNavigation::getNumberOfSeconds));
-        for (UserNavigation i : lst) System.out.println(i);
+        mL.run();
     }
 }
